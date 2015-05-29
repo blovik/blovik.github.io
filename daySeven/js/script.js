2844516f18bde1979baa615efb466631133a5f43
brian <blovikbiz@gmail.com>
2015-05-29 15:51:36 -0700

    javascript1, day 7 first commit

---
+++ daySeven/js/script.js
@@ -0,0 +1,100 @@
+
+//alert('hey');
+// number input================================
+
+function inputTop(topNumber) { //create top number
+	var oldNumber = document.getElementById("topNumber").innerHTML;
+	newNumber = oldNumber + topNumber;
+	document.getElementById("topNumber").innerHTML = newNumber;
+}
+
+function inputMiddle(middleNumber) { // create second number
+	var oldNumber = document.getElementById("middleNumber").innerHTML;
+	newNumber = oldNumber + middleNumber;
+	document.getElementById("middleNumber").innerHTML = newNumber;
+}
+
+function sign(s) { // set the operator
+	document.getElementById("sign").innerHTML = s;
+}
+
+function maths() {
+	var num1 = document.getElementById("topNumber").innerHTML;
+	var num2 = document.getElementById("middleNumber").innerHTML;
+	var s = document.getElementById("sign").innerHTML;
+	var answer = num1.toString() + s + num2.toString();
+	alert(answer);
+	document.getElementById("answer").innerHTML = eval(answer);
+}
+
+// calculator function=========================
+
+// function addIt(equate) {
+// 	//var answer = parseInt(num1) + parseInt(num2);
+// 	//alert(num1 + " " + numb2 + " " + answer);
+// 	alert("I'm adding!");
+// 	var answer = eval(equate);
+// 	document.getElementById("answer").innerHTML = answer;
+// }
+
+// function subtract(num1, num2) {
+//    var answer = num1 - num2;
+// 	document.getElementById("answer").innerHTML = answer;
+// }
+
+// function multiply(num1, num2) {
+//    var answer = num1 * num2;
+// 	document.getElementById("answer").innerHTML = answer;
+// }
+
+// function divide(num1, num2) {
+//    if(num1 === 0 || num2 === 0) {
+//       alert("Zero cannot be used in division.\nYou must start over!");
+//       maths();
+//    }
+//    var answer = num1 / num2;
+//    answer = answer.toFixed(2);
+// 	document.getElementById("answer").innerHTML = answer;}
+
+// function maths(operator) {
+// 	var number1 = document.getElementById("topNumber").innerHTML;
+// 	var number2 = document.getElementById("middleNumber").innerHTML;
+
+// 	parseInt(number1);
+// 	parseInt(number2);
+
+// 	var equation = num1 + operator + num2);
+// 	alert(equation);
+
+// 	alert("maths() see the following: " + operator + " " + number1 + " " + number2);
+
+// 	addIt();
+
+	// if(operator === "add") {
+	// 	addIt(number1, number2);
+	// } else if(operator === "subtract") {
+	// 	subtract(number1, number2);
+	// } else if(operator === "multiply") {
+	// 	multiply(number1, number2);
+	// } else if(operator === "divide") {
+	// 	divide(number1, number2);
+	// } else 
+
+	// switch(operator) {
+	// 	case "add":
+	// 		addIt(number1, number2);
+	// 		break;
+	// 	case "subtract":
+	// 		subtract(number1, number2);
+	// 		break;
+	// 	case "mulitply":
+	// 		multiply(number1, number2);
+	// 		break;
+	// 	case "divide":
+	// 		divide(number1, number2);
+	//}
+//    
+//    
+// }
+//        
+// maths();
